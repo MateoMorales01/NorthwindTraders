cb9434b32a8e3aabdd408a2cc8cdb279c487c4ac
Mateo Morales <Moralmat76@gmail.com>
2025-12-02 15:57:41 -0600

    try/catch methods added

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -6,46 +6,81 @@ public class App {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/northwind";
         String username = args[0];
-        String password = args [1];
+        String password = args[1];
 
-        Connection connection = DriverManager.getConnection(url,username,password);
+        displayProducts(url, username, password);
+        displayCustomers(url, username, password);
+    }
 
 
-        // define your query
+    private static void displayProducts(String url, String username, String password) {
+        displayProduct(url, username, password);
+    }
+
+    private static void displayProduct(String url, String username, String password) {
         String query = """
-                        SELECT ProductID, ProductName, UnitPrice, UnitsInStock
-                        FROM products
-                        Where ProductName Like ?
-                        """;
+                    SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+                    FROM products
+                    Where ProductName Like ?
+                    """;
 
-        // create statement
-        // the statement is tied to the open connection
-        PreparedStatement statement = connection.prepareStatement(query);
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             PreparedStatement statement = connection.prepareStatement(query)) {
 
-        String searchTerm = "%Cha%";
-        //set parameters
-        statement.setString(1,searchTerm);
+            String searchTerm = "%Cha%";
+            statement.setString(1, searchTerm);
 
-        // 2. Execute your query
-        ResultSet results = statement.executeQuery();
+            try (ResultSet results = statement.executeQuery()) {
 
-        // process the results
-        while (results.next()) {
-            int ProductID = results.getInt("ProductID");
-            String ProductName = results.getString("ProductName");
-            double UnitPrice = results.getDouble("UnitPrice");
-            int UnitsInStock = results.getInt("UnitsInStock");
+                while (results.next()) {
+                    int productID = results.getInt("ProductID");
+                    String productName = results.getString("ProductName");
+                    double unitPrice = results.getDouble("UnitPrice");
+                    int unitsInStock = results.getInt("UnitsInStock");
 
-            System.out.println(ProductID);
-            System.out.println(ProductName);
-            System.out.println(UnitPrice);
-            System.out.println(UnitsInStock);
-            System.out.println("-----------------------");
+                    System.out.println(productID);
+                    System.out.println(productName);
+                    System.out.println(unitPrice);
+                    System.out.println(unitsInStock);
+                    System.out.println("-----------------------");
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("There was an error retrieving the data. Please try again.");
+            e.printStackTrace();
         }
+    }
 
-        results.close();
-        statement.close();
-        connection.close();
+    private static void displayCustomers(String url, String username, String password) {
+        String query = """
+                SELECT ContactName, CompanyName, City, Country, Phone
+                FROM customers
+                Order by Country;
+                """;
 
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement statement = connection.prepareStatement(query)) {
+
+            try (ResultSet results = statement.executeQuery()) {
+
+                while (results.next()) {
+                    String contactName = results.getString("ContactName");
+                    String companyName = results.getString("CompanyName");
+                    String city = results.getString("City");
+                    String country = results.getString("Country");
+                    String phone = results.getString("Phone");
+
+                    System.out.println(contactName);
+                    System.out.println(companyName);
+                    System.out.println(city);
+                    System.out.println(country);
+                    System.out.println(phone);
+                    System.out.println("-----------------------");
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("There was an error retrieving the data. Please try again.");
+            e.printStackTrace();
+        }
     }
 }
